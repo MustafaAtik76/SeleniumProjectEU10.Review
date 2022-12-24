fef7869f4df6ad96e44c1a76f1dac23950b7a1d0
Mustafa Atik <mustafaatik76@gmail.com>
2022-12-25 00:27:55 +0300

    FirstSeleniumClass created

---
+++ src/test/java/week1/FirstSeleniumClass.java
@@ -0,0 +1,21 @@
+package week1;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstSeleniumClass {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.navigate().to("https://practice.cydeo.com"); //driver.get("https://www.google.com");
+
+
+    }
+}
